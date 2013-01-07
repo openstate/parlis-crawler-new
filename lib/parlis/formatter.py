4f74b414176a0813f75496f6b5c4686a67704427
Breyten Ernsting <bje@dds.nl>
2013-01-07 12:25:38 +0100

    Fix tabs and spaces ...

--- lib/parlis/formatter.py
+++ lib/parlis/formatter.py
@@ -11,32 +11,32 @@ logger = logging.getLogger(__name__)
 
 
 class ParlisBaseFormatter(object):
-	properties = []
+    properties = []
 
-	def __init__(self, properties):
-		self.properties = properties
+    def __init__(self, properties):
+        self.properties = properties
 
-	def format(self, rows, entity, relation=None):
-		raise NotImplementedError
+    def format(self, rows, entity, relation=None):
+        raise NotImplementedError
 
-		
+        
 class ParlisTSVFormatter(ParlisBaseFormatter):
-	def format(self, rows, entity, relation=None, path='.'):
-	    makedirs(path)
+    def format(self, rows, entity, relation=None, path='.'):
+        makedirs(path)
 
-		if relation is not None:
-			filename = '%s/%s_%s.tsv' % (path, entity, relation)
-		else:
-			filename = '%s/%s.tsv' % (path, entity)
-		
-		if not os.path.isfile(filename):
-			f = codecs.open(filename, 'w', 'utf-8')
-			f.write(u"\t".join(self.properties) + "\n")
-		else:
-			f = codecs.open(filename, 'a', 'utf-8')
+        if relation is not None:
+            filename = '%s/%s_%s.tsv' % (path, entity, relation)
+        else:
+            filename = '%s/%s.tsv' % (path, entity)
+        
+        if not os.path.isfile(filename):
+            f = codecs.open(filename, 'w', 'utf-8')
+            f.write(u"\t".join(self.properties) + "\n")
+        else:
+            f = codecs.open(filename, 'a', 'utf-8')
 
-		for row in rows:
-			row_items = [tsv_escape(row[item_prop]) for item_prop in self.properties]
-			f.write(u"\t".join(row_items) + "\n")
+        for row in rows:
+            row_items = [tsv_escape(row[item_prop]) for item_prop in self.properties]
+            f.write(u"\t".join(row_items) + "\n")
 
-		f.close()
+        f.close()
