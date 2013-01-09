1fd2a683fcf6fc668c063679b04fef8d17299b29
Breyten Ernsting <bje@dds.nl>
2013-01-09 17:12:34 +0100

    Actually compress files.

--- lib/parlis/compressor.py
+++ lib/parlis/compressor.py
@@ -2,7 +2,7 @@ import sys
 import os
 import re
 import logging
-from zipfile import ZipFile
+from zipfile import ZipFile, ZIP_DEFLATED
 
 logger = logging.getLogger(__name__)
 
@@ -16,6 +16,6 @@ class ParlisZipCompressor(ParlisBaseCompressor):
     def compress(self, file_name, file_list):
         logger.info("Compressing %s (%s)", file_name, file_list)
 
-        with ZipFile(file_name, 'w') as zip_file:
+        with ZipFile(file_name, 'w', ZIP_DEFLATED) as zip_file:
             for tsv_file in file_list:
                 zip_file.write(tsv_file, os.path.basename(tsv_file))
