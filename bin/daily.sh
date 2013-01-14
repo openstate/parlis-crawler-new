a27ffc7ac63c3de91450c4323db7b7b366760489
Breyten Ernsting <bje@dds.nl>
2013-01-14 12:16:22 +0100

    Daily update should do stuff from yesterday.

--- bin/daily.sh
+++ bin/daily.sh
@@ -7,18 +7,20 @@ PARLIS_VIRTUAL_ENV=parlis_new
 cd $PARLIS_CRAWLER_HOME
 workon $PARLIS_VIRTUAL_ENV
 
+YESTERDAY=`date -d yesterday '+%Y-%m-%d'`
+
 # Zaken
-./bin/crawler.py
+./bin/crawler.py -f $YESTERDAY -t $YESTERDAY
 # Stemmingen
-./bin/crawler.py -e Stemmingen
+./bin/crawler.py -e Stemmingen -f $YESTERDAY -t $YESTERDAY
 # Besluiten
-./bin/crawler.py -e Besluiten
+./bin/crawler.py -e Besluiten -f $YESTERDAY -t $YESTERDAY
 # Documenten
-./bin/crawler.py -e Documenten
+./bin/crawler.py -e Documenten -f $YESTERDAY -t $YESTERDAY
 # Activiteiten
-./bin/crawler.py -e Activiteiten
+./bin/crawler.py -e Activiteiten -f $YESTERDAY -t $YESTERDAY
 # Reserveringen
-./bin/crawler.py -e Reserveringen
+./bin/crawler.py -e Reserveringen -f $YESTERDAY -t $YESTERDAY
 
 cd "$PARLIS_CRAWLER_HOME/output"
 find . -name '*.zip' -exec mv \{\} $PARLIS_HTDOCS_UPDATES \;
