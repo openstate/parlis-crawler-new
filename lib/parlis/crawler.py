0d86700a309f58ff857ec89fc1ab2c8524330f4a
Breyten Ernsting <bje@dds.nl>
2013-01-23 12:31:30 +0100

    Whoops.

--- lib/parlis/crawler.py
+++ lib/parlis/crawler.py
@@ -93,9 +93,9 @@ class ParlisCrawler(object):
         api = ParlisAPI('SOS', 'Open2012', cache)
 
         #for current_date in get_dates(self.start_date, self.end_date):
-        for current_date in [start_date]:
+        for current_date in [self.start_date]:
             #current_end_date = current_date + datetime.timedelta(days=1)
-            current_end_date = end_date
+            current_end_date = self.end_date
             cache.date_str = str(current_date)
             entity_count = 0
             last_items_fetched = 250
