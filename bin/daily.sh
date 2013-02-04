71588866e3438334d7d0a5e050faf557ff69acc4
Breyten Ernsting <bje@dds.nl>
2013-02-04 10:47:23 +0100

    Send output to log.

--- bin/daily.sh
+++ bin/daily.sh
@@ -10,17 +10,17 @@ workon $PARLIS_VIRTUAL_ENV
 YESTERDAY=`date -d yesterday '+%Y-%m-%d'`
 
 # Zaken
-./bin/crawler.py -f $YESTERDAY -t $YESTERDAY
+./bin/crawler.py -f $YESTERDAY -t $YESTERDAY 2>&1 >$PARLIS_CRAWLER_HOME/daily.log
 # Stemmingen
-./bin/crawler.py -e Stemmingen -f $YESTERDAY -t $YESTERDAY
+./bin/crawler.py -e Stemmingen -f $YESTERDAY -t $YESTERDAY 2>&1 >>$PARLIS_CRAWLER_HOME/daily.log
 # Besluiten
-./bin/crawler.py -e Besluiten -f $YESTERDAY -t $YESTERDAY
+./bin/crawler.py -e Besluiten -f $YESTERDAY -t $YESTERDAY 2>&1 >>$PARLIS_CRAWLER_HOME/daily.log
 # Documenten
-./bin/crawler.py -e Documenten -f $YESTERDAY -t $YESTERDAY
+./bin/crawler.py -e Documenten -f $YESTERDAY -t $YESTERDAY 2>&1 >>$PARLIS_CRAWLER_HOME/daily.log
 # Activiteiten
-./bin/crawler.py -e Activiteiten -f $YESTERDAY -t $YESTERDAY
+./bin/crawler.py -e Activiteiten -f $YESTERDAY -t $YESTERDAY 2>&1 >>$PARLIS_CRAWLER_HOME/daily.log
 # Reserveringen
-./bin/crawler.py -e Reserveringen -f $YESTERDAY -t $YESTERDAY
+./bin/crawler.py -e Reserveringen -f $YESTERDAY -t $YESTERDAY 2>&1 >>$PARLIS_CRAWLER_HOME/daily.log
 
 cd "$PARLIS_CRAWLER_HOME/output"
 find . -name '*.zip' -exec mv \{\} $PARLIS_HTDOCS_UPDATES \;
